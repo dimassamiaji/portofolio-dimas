481595f62aecf2baf076c36467ea6727acd84c9e
dimassamiaji <dimassamji@gmail.com>
2024-06-14 09:21:52 +0700

    update

--- src/components/EmailSection.jsx
+++ src/components/EmailSection.jsx
@@ -56,10 +56,10 @@ const EmailSection = () => {
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="github.com">
+          <Link href="ghttps://github.com/dimassamiaji">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
-          <Link href="linkedin.com">
+          <Link href="https://www.linkedin.com/in/dimas-samiaji-bb557b137/">
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
           </Link>
         </div>
